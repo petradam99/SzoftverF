a23729603c86c176f42a8ebbb3757a984f243b9f
petradam99 <56345770+petradam99@users.noreply.github.com>
2021-05-12 04:20:08 +0200

    Projekt alapok hozzáadása

---
+++ src/main/java/controller/AddController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.scene.input.MouseEvent;
+
+public class AddController {
+    public void returnToApp(MouseEvent mouseEvent) {
+    }
+
+    public void AddRow(MouseEvent mouseEvent) {
+    }
+}
---
+++ src/main/java/controller/StatController.java
@@ -0,0 +1,8 @@
+package controller;
+
+import javafx.scene.input.MouseEvent;
+
+public class StatController {
+    public void returnToApp(MouseEvent mouseEvent) {
+    }
+}
